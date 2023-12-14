1919cea689a86fb3af9fc65e8e9e839250ffa694
Khawlah Khalid <khawlah1920@hotmail.com>
2023-12-14 14:43:01 +0300

    Initial Commit

---
+++ NoteApp_SwiftData/NoteApp_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp_SwiftDataApp.swift
+//  NoteApp_SwiftData
+//
+//  Created by Khawlah Khalid on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
